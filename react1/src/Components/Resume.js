541d2d0e478ff16078ca0fa3cf060a75825ab143
hatem <hatemahmed2003@gmail.com>
2023-08-10 17:38:00 +0530

    Task-4

--- react1/src/Components/Resume.js
+++ react1/src/Components/Resume.js
@@ -1,10 +1,23 @@
 import "./Resume.css"
+import React, {useState} from 'react';
 
 const Resume = () => {
+    const [show , setshow] = useState(false)
+    function buttons1 () {
+    setshow(!show)
+    }
+
     return <div className="mainbox"><h1 className="res">Resume</h1>
                 <br></br>
                 <br></br>
-
+            <div className='yr'>
+            {
+                show && <div className='curr_year'>
+                    <p className='yrr'><b><i>I am studying third year</i></b></p>
+                </div>
+            }
+               <button type='button' onClick={buttons1} className='year'><b><i>Year</i></b></button>
+        </div>
             <div className="resmain">
                 <div className='resflexbox'>   
                 
